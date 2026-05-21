bf33b110f7ecca941c212d40952ac1cbfaf6669a
essam <essamelnaggar10@gmail.com>
2026-05-21 21:59:03 +0300

    fix

--- app.js
+++ app.js
@@ -11,19 +11,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/images", express.static("images"));
 
-const options = {
-    customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
-    customJs: [
-    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js'
-    ],
-    customSiteTitle: "Car Go API Docs",
-    swaggerOptions: {
-    persistAuthorization: true,
-    }
-};
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(openapiSpec, false, options));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(openapiSpec));
 
 const orderrouter = require("./routes/orderrouter");
 const userrouter = require("./routes/userrouter");
